d624512814957390a9e4acc270ee8786906cc32b
praveenvelankani <pvn405.reddy@gmail.com>
2018-05-23 09:27:09 +0530

    Addind JS file

---
+++ public/js/index.js
@@ -0,0 +1,26 @@
+var socket = io();
+
+socket.on('connect',function(){
+  console.log("connected to server");
+//to send the data
+  // socket.emit('createEmail',{
+  //   to:"test@example.com",
+  //   text:"checkinh client server connection"
+  // });
+
+  socket.emit('createMessage',{
+    to:"test@example.com",
+    text:"checkinh client server connection"
+  });
+});
+
+//
+socket.on('disconnect',function(){
+  console.log("Disconnected from server");
+});
+socket.on('newMessage',function(message){
+  console.log('New Messsage',message);
+});
+// socket.on('newEmail',function(email){
+//   console.log('New Email',email);
+// });
