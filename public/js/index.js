a007f1778774842606c272763b60fff7794f89bc
praveenvelankani <pvn405.reddy@gmail.com>
2018-05-26 14:21:12 +0530

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -1,39 +1,60 @@
 var socket = io();
 
-socket.on('connect',function(){
-  console.log("connected to server");
-//to send the data
-  // socket.emit('createEmail',{
-  //   to:"test@example.com",
-  //   text:"checkinh client server connection"
-  // });
-
-//   socket.emit('createMessage',{
-//     from:"Praveen",
-//     text:"checking client server connection"
-//   });
+socket.on('connect', function () {
+  console.log('Connected to server');
 });
-jQuery('#message-form').on('submit', function(e){
-  e.preventDefault();
-  socket.emit('createMessage',{
-    from: 'User',
-    text: jQuery('[name=message]').val()
-  },function(){
 
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
+});
+
+socket.on('newLocationMessage', function (message) {
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a target="_blank">My current location</a>');
+
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+
+  var messageTextbox = jQuery('[name=message]');
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: messageTextbox.val()
+  }, function () {
+    messageTextbox.val('')
   });
 });
-//
-socket.on('disconnect',function(){
-  console.log("Disconnected from server");
-});
 
-socket.on('newMessage',function(message){
-  // console.log('New Messsage',message);
-  var li = jQuery('<li></li>');
-  li.text(`${message.from}:${message.text}`);
-  jQuery('#messages').append(li);
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function () {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser.');
+  }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
+  navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function () {
+    locationButton.removeAttr('disabled').text('Send location');
+    alert('Unable to fetch location.');
+  });
 });
-// socket.on('newEmail',function(email){
-//   console.log('New Email',email);
-// });
--- server/server.js
+++ server/server.js
@@ -1,63 +1,39 @@
 const path = require('path');
-const express = require('express');
 const http = require('http');
+const express = require('express');
 const socketIO = require('socket.io');
-const publicPath = path.join(__dirname,'../public');
-// console.log(__dirname + '/../public' );
-// console.log(publicPath);
-const {generateMessage}= require('./utils/message');
+
+const {generateMessage, generateLocationMessage} = require('./utils/message');
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
-
 var io = socketIO(server);
+
 app.use(express.static(publicPath));
-io.on('connection',(socket)=>{
-  console.log("New User connected");
 
-//to send data to client
+io.on('connection', (socket) => {
+  console.log('New user connected');
 
-  /*socket.emit('newEmail',{
-    from: "pvn405.reddy@gmail.com",
-    text: "Trying socket.io"
-  });*/
-//to listen from client
-  // socket.on('createEmail',(email)=>{
-  //   console.log(email);
-  // });
-  socket.emit('newMessage',generateMessage('Admin','welcome to chat app')
-  // {
-  //   from: 'Admin',
-  //   text: 'Welcome to the chat app'  }
-  );
-  socket.broadcast.emit('newMessage',{
-    from: 'Admin',
-    text: 'New user joined',
-    createdAt: new Date().getTime()
-  })
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-  socket.on('createMessage',(message)=>{
-    console.log(message);
-    io.emit('newMessage',{
-      text:message.text,
-      from:message.from,
-      cretaedAt: new Date().getTime()
-    });
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-    // socket.broadcast.emit('newMessage',{
-    //   from: message.from,
-    //   text: message.text,
-    //   cretaedAt: new Date().getime()
-    // });
+  socket.on('createMessage', (message, callback) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback();
   });
-  socket.on('disconnect',()=>{
-    console.log('User was disconencted');
-  })
+
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
 });
 
-// socket.on('disconnect',()=>{
-//   console.log("User was disconnected");
-// });
-server.listen(port,() => {
-  console.log('server started');
+server.listen(port, () => {
+  console.log(`Server is up on ${port}`);
 });
