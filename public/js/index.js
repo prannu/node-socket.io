5f8993ad25757dee995e33f23ee197e7e9956445
praveenvelankani <pvn405.reddy@gmail.com>
2018-05-28 08:57:37 +0530

    Scroll to bottom if useris close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -7,6 +7,21 @@ socket.on('connect', function () {
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
+// autoScroll function
+function scrollToBottom(){
+  //selectros
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+  //heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+}
 
 socket.on('newMessage', function (message) {
   var template = jQuery('#message-template').html();
@@ -17,6 +32,7 @@ socket.on('newMessage', function (message) {
     createdAt: formattedTime
   });
   jQuery('#messages').append(html);
+  scrollToBottom();
   // console.log('newMessage', message);
   // var formattedTime = moment(message.createdAt).format('h:mm a');
   // var li = jQuery('<li></li>');
@@ -41,6 +57,7 @@ socket.on('newLocationMessage', function (message) {
   // a.attr('href', message.url);
   // li.append(a);
   jQuery('#messages').append(li);
+  scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
